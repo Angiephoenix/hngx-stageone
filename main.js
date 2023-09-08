d8dceb0142f96ce317a1944bee84d05b578fa56d
Angiephoenix <angiephoenix93@gmail.com>
2023-09-08 21:27:24 +0100

    Personal and Real-Time Data Attributes

--- main.js
+++ main.js
@@ -2,17 +2,15 @@ function updateCurrentDayOfTheWeek() {
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDate = new Date();
     const currentDay = daysOfWeek[currentDate.getUTCDay()];
-    document.getElementById("currentDayOfTheWeek").textContent = currentDay;
+    document.getElementById("dayOfTheWeek").textContent = currentDay;
 }
 
 function updateCurrentUTCTime() {
     const currentUTCTime = new Date().getTime();
-    document.getElementById("currentUTCTime").textContent = currentUTCTime + " ms";
+    document.getElementById("UTCTime").textContent = currentUTCTime + " ms";
 }
 
-// Update current day and time on page load
 updateCurrentDayOfTheWeek();
 updateCurrentUTCTime();
 
-// Update current time every second
 setInterval(updateCurrentUTCTime, 1000);
